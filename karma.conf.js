4f43401b8d89546fe733a735c729380322bde896
Maxime Lafarie <maxime.lafarie@biig.fr>
2019-06-26 14:40:22 +0200

    Project build fixes

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ngx-smart-form'),
+      dir: require('path').join(__dirname, './coverage/angular-dynamic-forms'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
